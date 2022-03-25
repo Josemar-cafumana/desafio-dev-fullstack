95656203a840909cfa4f5d185493ba4e57b5f5a6
Josemar Cafumana <rjosemar-cafumana@hotmail.com>
2022-03-25 12:31:42 +0100

    Download de pacotes via composer

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+
+use CoffeeCode\Router\Router;
+
+$router = new Router(ROOT);
+
+$router->namespace("Source\Controllers")->group(null);
+
+$router->get("/", "Web:home", "web.home");
+$router->get("/importar", "Web:importar", "web.importar");
+$router->get("/transacao/{loja}", "Web:loja", "web.loja");
+$router->post("/create", "Web:store", "web.store");
+
+$router->dispatch();
+
